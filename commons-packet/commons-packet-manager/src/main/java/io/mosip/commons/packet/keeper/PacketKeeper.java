f628d847f273240815d3537ba89be483c6b1973e
khuddus shariff <khuddusshariff0022@gmail.com>
2025-05-12 11:10:40 +0530

    testing with loggs
    Signed-off-by: khuddus shariff <khuddusshariff0022@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
@@ -115,7 +115,9 @@ public class PacketKeeper {
      * @return
      */
     public boolean checkSignature(Packet packet, byte[] encryptedSubPacket) throws NoSuchAlgorithmException {
-        LOGGER.info("Packet Signature ===> " + packet.getPacketInfo().getSignature());
+        LOGGER.info("Packet Signature ===> " + CryptoUtil.decodeURLSafeBase64(packet.getPacketInfo().getSignature()).toString());
+        LOGGER.info("Ref ID ===> "+helper.getRefId(
+                packet.getPacketInfo().getId(), packet.getPacketInfo().getRefId()));
         boolean result = disablePacketSignatureVerification ? true :
         		getCryptoService().verify(helper.getRefId(
                         packet.getPacketInfo().getId(), packet.getPacketInfo().getRefId()), packet.getPacket()
